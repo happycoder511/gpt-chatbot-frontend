9f84d4ec12b40c27be38e7c5141c46ba31916350
happycoder511 <gnidenkobogdan67@gmail.com>
2023-04-24 15:26:51 +0300

    Frontend 1.6

--- src/components/burgerMenu/BurgerMenuHeader.jsx
+++ src/components/burgerMenu/BurgerMenuHeader.jsx
@@ -1,9 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import '../../styles/burgerMenu/burgerMenuHeader.scss'
+import Modal from 'react-modal';
 import { Context } from "../..";
 
 const BurgerMenuHeader = () => {
     const {user} = useContext(Context)
+    const [settingsOpen, setSettingsOpen] = useState(false)
+    function closeModal() {
+        setSettingsOpen(false);
+    }
 
     return (
         <div className="burger-menu-header-container">
@@ -11,7 +16,18 @@ const BurgerMenuHeader = () => {
                 <h1>My Journey</h1>
                 <p>The co-pilot for your career. Discover your path, shape your future. Choose your career with confidence.</p>
             </div>
-            <img src="/icons/settings-1.svg" alt="icon" className={!user.isAuth && 'hide-settings'}/>
+            <img src="/icons/settings-1.svg" alt="icon" className={!user.isAuth && 'hide-settings'} onClick={() => setSettingsOpen(true)}/>
+            {/* <Modal
+                isOpen={settingsOpen}
+                onRequestClose={closeModal}
+                ariaHideApp={false}
+                overlayClassName="modal-overlay-settings"
+                className="modal-settings"
+            >
+                <div className="sign-in-modal-send">
+                    <img src="/icons/settings-1.svg" alt="icon" className={!user.isAuth && 'hide-settings'}/>
+                </div>
+            </Modal> */}
         </div>
     );
 }
